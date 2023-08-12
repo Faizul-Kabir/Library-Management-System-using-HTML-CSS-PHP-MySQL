c6408e93fccbf211ee5ae7440777573aab0a80b2
Faizul-Kabir <142076035+Faizul-Kabir@users.noreply.github.com>
2023-08-12 12:56:52 +0600

    Add files via upload

---
+++ book.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html>
+
+<head>
+
+<title> Library </title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+
+<body>
+
+<h1> LIBRARY </h1></br></br>
+
+<p class="p1"> CHOOSE ANY BOOK YOU WANT </p>
+<p class="p5"> Collection of <i> HARRY POTTER </i>
+books</p><br>
+<br>
+<p class="p2">HARRY POTTER AND THE DEATHLY HALLOWS</br>
+<img class="image1" src ="potter.jpg" width="200" height="200">
+<a href=form.php>BURROW</a></p>
+<p class="p3">HARRY POTTER AND THE GOBLET OF FIRE<br>
+<img class="image2" src ="potter2.jpg" width="200" height="200">
+<a href=form.php>BURROW</a></p>
+<p class="p4">HARRY POTTER AND THE HALF BLOOD PRINCE<br>
+<img class="image3" src ="potter3.jpg" width="200" height="200">
+<a href=form.php>BURROW</a></p>
+
+
+
+</body>
+
+</html>
